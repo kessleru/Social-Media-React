da2e76b69f0aeceb1ab3fc0c4114f8944883d7aa
kessleru <otaviokesslerustra@hotmail.com>
2026-01-05 00:19:38 -0300

    feat: implement UserContext for user authentication and manage user state

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,14 +1,32 @@
 import { Link } from 'react-router-dom';
 import Dogs from '../Assets/dogs.svg?react';
+import { UserContext } from '../UserContext';
+import { useContext } from 'react';
 
 const Header = () => {
+  const { data } = useContext(UserContext);
+
   return (
-    <header  className='shadow-sm fixed w-full z-50 bg-white top-0'>
+    <header className='shadow-sm fixed w-full z-50 bg-white top-0'>
       <nav className='container flex justify-between items-center h-16'>
         <Link className='inline-block py-2' to='/' aria-label='Dogs - Home'>
-          <Dogs/>
+          <Dogs />
         </Link>
-        <Link className='text-neutral-800 flex items-center after:content-[""] after:inline-block after:ml-2 after:w-[14px] after:h-[17px] after:bg-[url("../Assets/usuario.svg")] after:bg-no-repeat after:bg-center after:relative after:-top-px' to='login'>Login / Criar</Link>
+        {data ? (
+          <Link
+            className='text-neutral-800 flex items-center after:content-[""] after:inline-block after:ml-2 after:w-[14px] after:h-[17px] after:user-icon after:bg-no-repeat after:bg-center after:relative after:-top-0.5px'
+            to='/conta'
+          >
+            {data.nome}
+          </Link>
+        ) : (
+          <Link
+            className='text-neutral-800 flex items-center after:content-[""] after:inline-block after:ml-2 after:w-[14px] after:h-[17px] after:user-icon after:bg-no-repeat after:bg-center after:relative after:-top-0.5px'
+            to='/login'
+          >
+            Login / Criar
+          </Link>
+        )}
       </nav>
     </header>
   );
--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -2,40 +2,20 @@ import { Link } from 'react-router-dom';
 import Input from '../Forms/Input';
 import Button from '../Forms/Button';
 import useForm from '../../Hooks/useForm';
-import { TOKEN_POST, USER_GET } from '../../api';
-import { useCallback, useEffect } from 'react';
+import { useContext } from 'react';
+import { UserContext } from '../../UserContext';
 
 const LoginForm = () => {
   const username = useForm();
   const password = useForm();
 
-  const getUser = useCallback(async (token) => {
-    const { url, options } = USER_GET(token);
-    const response = await fetch(url, options);
-    const json = await response.json();
-    console.log(json);
-  }, []);
-
-  useEffect(() => {
-    const token = window.localStorage.getItem('token');
-    if (token) {
-      getUser(token);
-    }
-  }, [getUser]);
+  const { userLogin } = useContext(UserContext);
 
   async function handleSubmit(event) {
     event.preventDefault();
 
     if (username.validate() && password.validate()) {
-      const { url, options } = TOKEN_POST({
-        username: username.value,
-        password: password.value,
-      });
-
-      const response = await fetch(url, options);
-      const json = await response.json();
-      window.localStorage.setItem('token', json.token);
-      getUser(json.token);
+      userLogin(username.value, password.value);
     }
   }
 
