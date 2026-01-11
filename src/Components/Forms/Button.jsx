2f07a1de1cd7736502d404d8495783b6d8923cea
kessleru <otaviokesslerustra@hotmail.com>
2026-01-10 21:11:21 -0300

    feat: update Button and LoginForm styles for improved accessibility and consistency

--- src/Components/Forms/Button.jsx
+++ src/Components/Forms/Button.jsx
@@ -2,7 +2,7 @@ const Button = ({ children, ...props }) => {
   return (
     <button
       {...props}
-      className='font-first text-base cursor-pointer rounded-md bg-amber-400 text-amber-950 py-3 px-5 min-w-32 transition duration-150 select-none hover:shadow-glow focus:shadow-glow focus:outline-none disabled:opacity-50 disabled:cursor-wait disabled:shadow-none active:scale-95'
+      className='font-first text-sm font-bold cursor-pointer rounded-md bg-amber-400 text-amber-900 tracking-wider py-3 px-5 min-w-32 transition duration-150 select-none uppercase hover:shadow-glow focus:shadow-glow focus:outline-none disabled:opacity-50 disabled:cursor-wait disabled:shadow-none active:scale-95'
     >
       {children}
     </button>
--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -48,7 +48,7 @@ const LoginForm = () => {
         <p className='mt-4'>Ainda não possue conta? Cadastrar-se no site.</p>
         <Link
           to='/login/criar'
-          className='inline-block text-center font-first text-base cursor-pointer rounded-md bg-amber-400 text-amber-950 py-3 px-5 min-w-32 mt-4 sm:mt-8 transition duration-150 select-none hover:shadow-glow focus:shadow-glow focus:outline-none disabled:opacity-50 disabled:cursor-wait disabled:shadow-none'
+          className='inline-block text-amber-900 tracking-wider font-bold text-center font-first text-base cursor-pointer rounded-md bg-amber-400 py-3 px-5 min-w-32 mt-4 sm:mt-8 transition duration-150 select-none hover:shadow-glow focus:shadow-glow focus:outline-none disabled:opacity-50 disabled:cursor-wait disabled:shadow-none'
         >
           Cadastro
         </Link>
